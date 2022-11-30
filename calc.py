ec06ff8f635966cc91e5586a5334485d1ac6fe8a
Sufiyan <sufiyan.in.tech@gmail.com>
2022-11-30 21:15:11 +0530

    wrote the show, clear and solve methods

--- calc.py
+++ calc.py
@@ -6,9 +6,26 @@ class Calculator:
     def __init__(self, master):
         master.title("Calculator")
         master.geometry('357x420+0+0')
-        master.config(bg='black')
-        master.resizeable(False, False)
+        master.config(bg='white')
+        master.resizable(False, False)
 
         self.equation = StringVar()
         self.entry_value = ''
-        Entry(Width=17, bg='#fff', font=('Arial Bold', 28), textvariable=self.equation).place(x=0, y=0)
+        Entry(width=17, bg='gray', font=('Arial Bold', 28), textvariable=self.equation).place(x=0, y=0)
+
+    def show(self, value):
+        self.entry_value += str(value)
+        self.equation.set(self.entry_value)
+
+    def clear(self):
+        self.entry_value = ''
+        self.equation.set(self.entry_value)
+
+    def solve(self):
+        result = eval(self.entry_value)
+        self.equation.set(result)
+
+
+root = Tk()
+calculator = Calculator(root)
+root.mainloop()
